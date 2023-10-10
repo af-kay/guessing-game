78f508bdec57d3106dec2f784d49cc3723ad88a0
A. F. Kay <afkay.dev@icloud.com>
2023-10-10 05:15:52 +0300

    Move initial game state to config

--- src/features/guessing-game/guessing-game.config.ts
+++ src/features/guessing-game/guessing-game.config.ts
@@ -1,9 +1,12 @@
 import { CardIconName } from '../../components/icon-card';
 
+import { GuessingGameSessionState } from './guessing-game.types';
+
 export const GUESSING_GAME_CONFIG = {
   cardsForSingleGuess: 2,
   cardsAmount: 16,
   autoSolveLastGuess: true,
   wrongGuessDisplayMs: 700, // in ms
   iconsToChooseFrom: Object.values(CardIconName),
+  initialGameState: GuessingGameSessionState.NOT_STARTED,
 };
--- src/features/guessing-game/guessing-game.context.tsx
+++ src/features/guessing-game/guessing-game.context.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import {
-  GuessingGameSession,
-  GuessingGameSessionState,
-} from './guessing-game.types';
+import { GuessingGameSession } from './guessing-game.types';
 import { makeGuessingGameCards } from './guessing-game.utils';
 import { useGameCards } from './game-cards';
 import { useGameState } from './game-state';
+import { GUESSING_GAME_CONFIG } from './guessing-game.config';
 
 export const GuessingGameContext = React.createContext(
   {} as GuessingGameSession,
@@ -15,7 +13,7 @@ export const GuessingGameContext = React.createContext(
 export const GuessingGameContextProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  const state = useGameState(GuessingGameSessionState.IN_PROGRESS);
+  const state = useGameState(GUESSING_GAME_CONFIG.initialGameState);
   const cards = useGameCards(makeGuessingGameCards);
 
   return (
