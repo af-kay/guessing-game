f7c5f0fdece1f8f76fc6e2f1c0e4f09ae42ac6b0
A. F. Kay <afkay.dev@icloud.com>
2023-10-10 04:00:36 +0300

    Move game board to component

---
+++ src/features/guessing-game/game-board/game-board.component.tsx
@@ -0,0 +1,18 @@
+import { Board } from '../../../components/board';
+
+import { GuessCard } from '../guess-card';
+import { useGuessingGame } from '../guessing-game.hook';
+
+export const GameBoard = () => {
+  const {
+    cards: { allCards },
+  } = useGuessingGame();
+
+  return (
+    <Board>
+      {allCards.map(card => (
+        <GuessCard key={card.id} id={card.id} />
+      ))}
+    </Board>
+  );
+};
--- src/features/guessing-game/guessing-game.component.tsx
+++ src/features/guessing-game/guessing-game.component.tsx
@@ -1,10 +1,7 @@
-import { Board } from '../../components/board';
+import { GameBoard } from './game-board';
 
 import { useGameLogic } from './game-logic';
-import { GuessCard } from './guess-card';
-
 import { GuessingGameContextProvider } from './guessing-game.context';
-import { useGuessingGame } from './guessing-game.hook';
 
 export const GuessingGame = () => (
   <GuessingGameContextProvider>
@@ -13,17 +10,7 @@ export const GuessingGame = () => (
 );
 
 const Game = () => {
-  const {
-    cards: { allCards },
-  } = useGuessingGame();
-
   useGameLogic();
 
-  return (
-    <Board>
-      {allCards.map(card => (
-        <GuessCard key={card.id} id={card.id} />
-      ))}
-    </Board>
-  );
+  return <GameBoard />;
 };
