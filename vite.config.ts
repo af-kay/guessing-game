3e2dc9abea55a3c62082e9ec6d91f4e686f2631a
A. F. Kay <afkay.dev@icloud.com>
2023-10-15 02:10:04 +0300

    Update vite config with gh-pages app base

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { ConfigEnv, defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const isProd = (mode: ConfigEnv['mode']) => mode === 'production';
+
+const getAppBase = (mode: ConfigEnv['mode']) =>
+  isProd(mode) ? '/guessing-game' : '';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }: ConfigEnv) => ({
   plugins: [react()],
+  base: getAppBase(mode),
   server: {
     port: 3000,
   },
-});
+}));
