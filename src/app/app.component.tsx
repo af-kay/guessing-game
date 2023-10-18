ae77074e160b178e316c94293ef9dd0f628f446e
A. F. Kay <afkay.dev@icloud.com>
2023-10-18 04:44:30 +0300

    Configure react-hot-toast

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,5 +1,12 @@
+import { Toaster } from 'react-hot-toast';
+
 import { GuessingGame } from '../features/guessing-game';
 
 export function App() {
-  return <GuessingGame />;
+  return (
+    <>
+      <GuessingGame />
+      <Toaster />
+    </>
+  );
 }
