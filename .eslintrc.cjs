31707a949809a0308cbfe55cb4d55285f219c04a
A. F. Kay <afkay.dev@icloud.com>
2023-10-20 04:31:50 +0300

    Update eslint config with _ for unused stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     ],
     'import/order': [1, { 'newlines-between': 'always' }],
     'import/newline-after-import': [1, { count: 1 }],
+    '@typescript-eslint/no-unused-vars': ["error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
 
     // TODO: enable and make exceptions for styled & react where needed
     'import/no-named-as-default': 0,
