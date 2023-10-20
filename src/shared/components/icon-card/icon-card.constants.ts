0c16b06230c637d33bc76f063f040c7c0f243c88
A. F. Kay <afkay.dev@icloud.com>
2023-10-20 06:27:45 +0300

    Update icon card sizings

--- src/shared/components/icon-card/icon-card.constants.ts
+++ src/shared/components/icon-card/icon-card.constants.ts
@@ -1,5 +1,5 @@
-export const ICON_CARD_SIZE = 102;
-export const ICON_CARD_ICON_SIZE = 64;
-export const ICON_CARD_CLOSED_ICON_SIZE = ICON_CARD_ICON_SIZE;
+export const ICON_CARD_SIZE = 112;
+export const ICON_CARD_ICON_SIZE = 86;
+export const ICON_CARD_CLOSED_ICON_SIZE = 72;
 
 export const ICON_CARD_BORDER_RADIUS = 8;
