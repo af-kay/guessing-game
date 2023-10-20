ee4225356471e9ebd6fa06a63fc7ef05702fd59d
A. F. Kay <afkay.dev@icloud.com>
2023-10-20 03:05:28 +0300

    Move board max width calculation to util

--- src/components/board/board.component.tsx
+++ src/components/board/board.component.tsx
@@ -7,8 +7,7 @@ import {
   BOARD_PADDING,
 } from './board.constants';
 import { IBoard } from './board.types';
-
-import { ICON_CARD_SIZE } from '$components/icon-card/icon-card.constants';
+import { getBoardMaxWidth } from './board.utils';
 
 export const Board = ({ maxColumns, children }: IBoard) => {
   return <Layout maxColumns={maxColumns}>{children}</Layout>;
@@ -16,10 +15,7 @@ export const Board = ({ maxColumns, children }: IBoard) => {
 
 const Layout = styled.div<Pick<IBoard, 'maxColumns'>>`
   display: flex;
-  max-width: ${p =>
-    ICON_CARD_SIZE * p.maxColumns! +
-    BOARD_GAP * (p!.maxColumns - 1) +
-    BOARD_PADDING * 2}px;
+  max-width: ${getBoardMaxWidth}px;
   align: auto;
   flex-wrap: wrap;
   gap: ${BOARD_GAP}px;
---
+++ src/components/board/board.utils.ts
@@ -0,0 +1,14 @@
+import { BOARD_GAP, BOARD_PADDING } from './board.constants';
+import { IBoard } from './board.types';
+
+import { ICON_CARD_SIZE } from '$components/icon-card/icon-card.constants';
+
+export const getBoardMaxWidth = ({
+  maxColumns,
+}: Pick<IBoard, 'maxColumns'>) => {
+  const cardsWidth = ICON_CARD_SIZE * maxColumns;
+  const cardsGap = BOARD_GAP * (maxColumns - 1);
+  const padding = BOARD_PADDING * 2;
+
+  return cardsWidth + cardsGap + padding;
+};
